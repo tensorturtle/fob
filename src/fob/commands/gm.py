tensorturtle/fob

commit ef7aa9cf283abedefcb941fa97008bfdeb6bd5ab

v0.2.11 Fix #10

src/fob/commands/gm.py
@@ -102,6 +102,10 @@ def new_day(args: Namespace, db: TinyDBWrapper, data) -> None:
 
         blocks_remaining_to_assign -= blocks_assigned
 
+        # end this command if we've assigned all blocks for today
+        if blocks_remaining_to_assign == 0:
+            break
+
         new_not_completed_areas[area_name] = {
             'allocated': blocks['allocated'],
             'completed': blocks['completed'] + int(blocks_assigned)

pyproject.toml
@@ -1,7 +1,7 @@
 [project]
 name = "focus-blocks"
 # Remember to update the version number in 'utils/utils.py' manually also.
-version = "0.2.10"
+version = "0.2.11"
 description = "Focus Blocks: Simple and elegant daily time management tool."
 readme = "README.md"
 authors = [{ name = "tensorturtle", email = "tensorturtle@gmail.com" }]

src/fob/utils/utils.py
@@ -7,7 +7,7 @@ import sys
 from rich import print
 
 def get_version_number() -> tuple[int, int, int]:
-    return (0,2,10)
+    return (0,2,11)
 
 def get_db_path(args: Namespace) -> Path:
     '''

commit 58da97a81dafc47fba9f9966f7e485ca2d14b01d

v0.2.12 fix off-by-one error introduced in ef7aa9c

src/fob/commands/gm.py
@@ -102,15 +102,15 @@ def new_day(args: Namespace, db: TinyDBWrapper, data) -> None:
 
         blocks_remaining_to_assign -= blocks_assigned
 
-        # end this command if we've assigned all blocks for today
-        if blocks_remaining_to_assign == 0:
-            break
-
         new_not_completed_areas[area_name] = {
             'allocated': blocks['allocated'],
             'completed': blocks['completed'] + int(blocks_assigned)
         }
         today_areas[area_name] = blocks_assigned
+
+        # end this command if we've assigned all blocks for today
+        if blocks_remaining_to_assign == 0:
+            break
 
     if blocks_remaining_to_assign > 0:
         print("[red][bold]Error![/red][/bold] You have not assigned all your blocks. Please try again.")